faf548b78b7772343fd38586f0f7d1c40c3bffd8
Ryan Stanford <ryanstanford92@gmail.com>
2024-08-01 01:50:15 -0500

    Refactor grid to make it larger

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-//container for 16x16 grid of square divs
-//use js and flexbox to create the grid
 let gridContainer = document.querySelector(".grid-container"); 
 
+//create 16x16 grid 
 for (let index = 0; index < 16; index++) {
 
     let column = document.createElement("div");
 
     for (let index = 0; index < 16; index++) {
         let squareDiv = document.createElement("div");
-        squareDiv.setAttribute("style", "height: 10px; width: 10px;");
+        squareDiv.setAttribute("style", "height: 30px; width: 30px;");
         column.appendChild(squareDiv);
      }
      
